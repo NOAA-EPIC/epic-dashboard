2d5cdfa98d0808c20fbfb1535232b3f3fc8db08e
ankimball <131040494+ankimball@users.noreply.github.com>
2024-01-15 10:52:54 -0600

    inital additions to CICD artifacts

--- src/CICDDashboard.js
+++ src/CICDDashboard.js
@@ -1,9 +1,56 @@
+// import React, { useEffect, useState } from "react";
+// import ArtifactDataGrid from "./ArtifactDataGrid";
+// import "./App.css";
+
+
+// const artifact_data_endpoint = [
+//   "https://noaa-epic-prod-jenkins-public-react.s3.amazonaws.com/ufs-srweather-app-dashboard.json",
+// ];
+
+// function removeDashboard(title) {
+//   return title.replace(/-dashboard.*$/i, "").trim();
+// }
+
+// function CICDpiepline() {
+//   const [tableTitles, setTableTitles] = useState([]);
+
+//   useEffect(() => {
+//     const extractProjectName = (url) => {
+//       const lastSlashIndex = url.lastIndexOf("/");
+//       const fileName = url.substring(lastSlashIndex + 1);
+//       const dotIndex = fileName.lastIndexOf(".");
+//       const projectName = fileName.substring(0, dotIndex);
+//       return projectName;
+//     };
+
+//     const titles = artifact_data_endpoint.map((endpoint) =>
+//       removeDashboard(extractProjectName(endpoint))
+//     );
+//     setTableTitles(titles);
+//   }, []);
+
+//   return (
+//     <div style={{ padding: 30 }}>
+//       {tableTitles.map((title, index) => (
+//         <div key={index}>
+//           <h1>{title} CI/CD Artifacts</h1>
+//           <ArtifactDataGrid endpoints={[artifact_data_endpoint[index]]} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default CICDpiepline;
+
+
 import React, { useEffect, useState } from "react";
 import ArtifactDataGrid from "./ArtifactDataGrid";
 import "./App.css";
 
-
-const artifact_data_endpoint = [
+const artifact_data_endpoints = [
+  "https://noaa-epic-prod-jenkins-public-react.s3.amazonaws.com/ufs-srweather-app-dashboard.json",
+  "https://noaa-epic-prod-jenkins-public-react.s3.amazonaws.com/ufs-srweather-app-dashboard.json", 
   "https://noaa-epic-prod-jenkins-public-react.s3.amazonaws.com/ufs-srweather-app-dashboard.json",
 ];
 
@@ -23,7 +70,7 @@ function CICDpiepline() {
       return projectName;
     };
 
-    const titles = artifact_data_endpoint.map((endpoint) =>
+    const titles = artifact_data_endpoints.map((endpoint) =>
       removeDashboard(extractProjectName(endpoint))
     );
     setTableTitles(titles);
@@ -31,10 +78,10 @@ function CICDpiepline() {
 
   return (
     <div style={{ padding: 30 }}>
-      {tableTitles.map((title, index) => (
+      {artifact_data_endpoints.map((endpoint, index) => (
         <div key={index}>
-          <h1>{title} Build History</h1>
-          <ArtifactDataGrid endpoints={[artifact_data_endpoint[index]]} />
+          <h1>{tableTitles[index]} CI/CD Artifacts</h1>
+          <ArtifactDataGrid endpoints={[endpoint]} />
         </div>
       ))}
     </div>
