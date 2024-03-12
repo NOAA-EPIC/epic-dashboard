248e4ecf912d0c0946a9d646ba2f35c99b80f668
ankimball <131040494+ankimball@users.noreply.github.com>
2024-03-12 10:40:14 -0500

    fixing compile warnings

--- src/AllocationReport.js
+++ src/AllocationReport.js
@@ -3,19 +3,19 @@ import React, { useState, useEffect } from 'react';
 const HTMLLoader = ({ url }) => {
   const [htmlContent, setHtmlContent] = useState(null);
 
-  const loadHTML = async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.text();
-      setHtmlContent(data);
-    } catch (error) {
-      console.error('Error loading HTML:', error);
-    }
-  };
-
   useEffect(() => {
+    const loadHTML = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.text();
+        setHtmlContent(data);
+      } catch (error) {
+        console.error('Error loading HTML:', error);
+      }
+    };
+
     loadHTML();
-  }, []);
+  }, [url]); // Include url in the dependency array
 
   return (
     <div>
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import TestDashboard from "./TestDashboard";
-import { Box } from "@mui/material";
 
 function App() {
   return (
