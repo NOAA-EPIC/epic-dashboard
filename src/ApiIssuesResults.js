6594fa43f15ada8a0c9a8edc8ef610ea2ce6ec81
todd arbetter <todd.arbetter@noaa.gov>
2026-06-18 10:33:37 -0600

    Update issue description in ApiIssuesResults component

--- src/ApiIssuesResults.js
+++ src/ApiIssuesResults.js
@@ -6,9 +6,11 @@ const issueEndpoints =  [
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-hafs-community-HAFS.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-NOAA-EMC-UPP.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-NOAA-EPIC-land-offline_workflow.json',
+  'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-NOAA-EPIC-EAGLE.json',  
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-ufs-community-land-DA_workflow.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-ufs-community-ufs-srweather-app.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-ufs-community-ufs-weather-model.json',
+  'https://epic-health-dashboard-artifacts.s3.amazonaws.com/issues-ufs-community-ufs-da_workflow.json',
 ];
 
 function ApiIssuesResults() {
@@ -16,7 +18,7 @@ function ApiIssuesResults() {
     <div style={{ padding: 30 }}>
       <div>
         <h1>Issues</h1>
-        <p>Includes: ufs-srweather-app, ufs-weather-model, UPP, land-DA_workflow, HAFS, and NOAA-EPIC/land-offline_workflow</p>
+        <p>Includes: ufs-srweather-app, ufs-weather-model, ufs-da-workflow, UPP, land-DA_workflow, HAFS, NOAA-EPIC/EAGLE, and NOAA-EPIC/land-offline_workflow</p>
         <p><span style={{ color: 'red' }}>Red</span> indicates that there has been no initial reply to the author. Only issues labelled for EPIC support and issues created within the last two weeks are shown.</p>
         <IssueDataGrid endpoints={issueEndpoints} />
       </div>
@@ -25,4 +27,3 @@ function ApiIssuesResults() {
 }
 
 export default ApiIssuesResults;
-
