d884aeb33f130bc0f430af7879aeed2c7129455d
todd arbetter <todd.arbetter@noaa.gov>
2026-06-18 10:32:37 -0600

    Update discussion list and description in ApiDiscussionsResults

--- src/ApiDiscussionsResults.js
+++ src/ApiDiscussionsResults.js
@@ -6,9 +6,11 @@ const discussionEndpoints = [
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-hafs-community-HAFS.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-NOAA-EMC-UPP.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-NOAA-EPIC-land-offline_workflow.json',
+  'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-NOAA-EPIC-EAGLE.json',  
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-ufs-community-land-DA_workflow.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-ufs-community-ufs-srweather-app.json',
   'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-ufs-community-ufs-weather-model.json',
+  'https://epic-health-dashboard-artifacts.s3.amazonaws.com/discussions-ufs-community-ufs-da-workflow.json',  
 ];
 
 function ApiDiscussionResults() {
@@ -16,7 +18,7 @@ function ApiDiscussionResults() {
     <div style={{ padding: 30 }}>
       <div>
         <h1>Discussions</h1>
-        <p>Includes: ufs-srweather-app, ufs-weather-model, UPP, land-DA_workflow, HAFS, and NOAA-EPIC/land-offline_workflow</p>
+        <p>Includes: ufs-srweather-app, ufs-weather-model, ufs-da-workflow, UPP, land-DA_workflow, HAFS, NOAA-EPIC/EAGLE, and NOAA-EPIC/land-offline_workflow</p>
         <p><span style={{ color: 'green' }}>Green</span> indicates that the US team has replied most recently. <span style={{ color: 'red' }}>Red</span> indicates that there has been no initial reply to the author or that the author was the last to reply.</p>
         <p><a href="https://github.com/orgs/ufs-community/discussions">UFS Community Discussions</a></p>
         <ItemDataGrid endpoints={discussionEndpoints} />
